0854ac8dae4a3256789bda0a94c556165688e439
tyganit@gmail.com <tyganit@gmail.com@c389ed42-0118-07df-e2f5-b6c3ece5d433>
2013-10-25 21:31:03 +0000

    Version 0.1.3 (compatible win32)

--- trunk/trunk/main.lua
+++ trunk/trunk/main.lua
@@ -7,20 +7,14 @@ function love.load(arg)
 	love.graphics.setMode(Settings.ScreenWidth, Settings.ScreenHeight)
 
 	Note.Load()
-	testNote = Note.New(Note.A, 5, 5)
+	testNote = Note.New(1, 5, 5)
 end
 
 function love.keypressed(key)
 	if key == 'escape' then
 		love.event.push('quit')
-	elseif key == "1" then
-		GamePad:Onpress(0, GamePad.A)
-	elseif key == "2" then
-		GamePad:Onpress(0, GamePad.B)
-	elseif key == "3" then
-		GamePad:Onpress(0, GamePad.X)
-	elseif key == "4" then
-		GamePad:Onpress(0, GamePad.Y)
+	elseif key == 'return' then
+		testNote.state = (testNote.state + 1) % 4
 	elseif key == ' ' then
 		testNote.value = (testNote.value) % 4 + 1
 	end
@@ -28,21 +22,6 @@ end
 
 function love.keyreleased(key)
 
-end
-
-function love.joystickpressed(pad, button)
-	if button == 1 then
-		GamePad:OnPress(pad, GamePad.A)
-	elseif button == 2 then
-		GamePad:OnPress(pad, GamePad.B)
-	elseif button == 3 then
-		GamePad:OnPress(pad, GamePad.X)
-	elseif button == 4 then
-		GamePad:OnPress(pad, GamePad.Y)
-	end
-end
-
-function love.joystickreleased(pad, button)
 end
 
 function love.draw()

90413406972765c9236c55f6cedf0b1f66ab4bae
tyganit <tyganit@gmail.com>
2013-10-25 22:33:51 +0000

    First version of Stage.lua, integration with the NoteGenerator

--- trunk/Settings.lua
+++ trunk/Settings.lua
@@ -3,9 +3,10 @@ Settings = {}
 Settings.ScreenWidth  = 1280
 Settings.ScreenHeight =  720
 
+Settings.HitzoneWidthRatio = 0.15
+
 Settings.NoteStartLeftX = 0
 Settings.NoteStartRightX = Settings.ScreenWidth
 Settings.NoteMinOffsetY = 300
 Settings.NoteMaxOffsetY = 500
 Settings.NoteMarginY = 35
-

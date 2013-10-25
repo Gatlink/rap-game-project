1a9eff76c53f82658835adcaafb84f9cc4dd1184
patrick.rabier@gmail.com <patrick.rabier@gmail.com@c389ed42-0118-07df-e2f5-b6c3ece5d433>
2013-10-25 19:14:22 +0000

    love + main

---
+++ trunk/trunk/main.lua
@@ -0,0 +1,23 @@
+
+function love.load(arg)
+	
+end
+
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.push('quit')
+	end
+end
+
+function love.keyreleased(key)
+
+end
+
+function love.draw()
+	
+end
+
+function love.update(dt)
+
+
+end

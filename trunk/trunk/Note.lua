8274da8efc20d8b644244b2722d0fe336184d1ce
tyganit@gmail.com <tyganit@gmail.com@c389ed42-0118-07df-e2f5-b6c3ece5d433>
2013-10-26 09:45:17 +0000

    Note animation using loveanimation

--- trunk/trunk/Note.lua
+++ trunk/trunk/Note.lua
@@ -1,4 +1,5 @@
 require 'Settings'
+require 'loveanimation'
 require 'GamePad'
 
 Note = {}
@@ -10,10 +11,10 @@ local _speed = 100
 Note.Left  = -1
 Note.Right =  1
 
-Note.Passive = 0
-Note.Active  = 1
-Note.Hit     = 2
-Note.Miss    = 3
+Note.Passive = 1
+Note.Active  = 2
+Note.Hit     = 3
+Note.Miss    = 4
 
 function Note.New(value, x, y, dir, state)
 	local new = {}
@@ -23,29 +24,34 @@ function Note.New(value, x, y, dir, state)
 	new.y = y or 0
 	new.direction = dir or Note.Right
 	new.state = state or Note.Passive
+	new.sprite = _sprites[new.value]:clone()
+	new.sprite:setRelativeOrigin(0.5, 0.5)
 
 	setmetatable(new, Note)
 	return new
 end
 
 function Note.Load()
-	_sprites[GamePad.A] = love.graphics.newImage('assets/sprites/A.png')
-	_sprites[GamePad.B] = love.graphics.newImage('assets/sprites/B.png')
-	_sprites[GamePad.X] = love.graphics.newImage('assets/sprites/X.png')
-	_sprites[GamePad.Y] = love.graphics.newImage('assets/sprites/Y.png')
+	_sprites[GamePad.A] = LoveAnimation.new('assets/animations/note.lua')
+	_sprites[GamePad.B] = LoveAnimation.new('assets/animations/note.lua',love.graphics.newImage('assets/sprites/B.png'))
+	_sprites[GamePad.X] = LoveAnimation.new('assets/animations/note.lua',love.graphics.newImage('assets/sprites/X.png'))
+	_sprites[GamePad.Y] = LoveAnimation.new('assets/animations/note.lua',love.graphics.newImage('assets/sprites/Y.png'))
+end
+
+function Note:setState(state)
+	self.state = state
+	self.sprite:setState(tostring(state))
 end
 
 function Note:Draw()
-	love.graphics.setScissor(self.x - Settings.NoteSize / 2 , self.y - Settings.NoteSize / 2, Settings.NoteSize, Settings.NoteSize)
-	love.graphics.push()
-	love.graphics.translate(-Settings.NoteSize / 2, -Settings.NoteSize / 2)
-	love.graphics.draw(_sprites[self.value], self.x, self.y, 0, 1, 1, 0, self.state * Settings.NoteSize)
-	love.graphics.pop()
-	love.graphics.setScissor()
+	self.sprite:draw()
 end
 
 function Note:Update(dt)
 	if self.x <= Settings.ScreenWidth and self.x >= -Settings.NoteSize then
 		self.x = self.x + dt * self.direction * _speed
 	end
+
+	self.sprite:update(dt)
+	self.sprite:setPosition(self.x, self.y)
 end

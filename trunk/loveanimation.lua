88ec6feb8cf133d48583fbf51247e7214c303983
patrick.rabier <patrick.rabier@gmail.com>
2013-10-26 09:46:59 +0000

    sprite intersection

--- trunk/loveanimation.lua
+++ trunk/loveanimation.lua
@@ -251,6 +251,31 @@ function LoveAnimation:resetAnimation()
 
 end
 
+
+local _CheckCollision = function(ax1,ay1,aw,ah, bx1,by1,bw,bh)
+  local ax2,ay2,bx2,by2 = ax1 + aw, ay1 + ah, bx1 + bw, by1 + bh
+  return ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1
+end
+
+--
+--
+--
+--
+function LoveAnimation:intersects(x,y,width,height)
+	local h = height or 1
+	local w = width or 1
+	local state_descriptor = self.descriptor.states[self.currentState]
+
+	return _CheckCollision(
+		self.x - (self.relativeOriginX * state_descriptor.frameW)
+			+ state_descriptor.frameW / 4,
+		self.y - (self.relativeOriginY * state_descriptor.frameH),
+		state_descriptor.frameW - state_descriptor.frameW/4,
+		state_descriptor.frameH,
+		x,y,w,h
+	)
+end
+
 --
 --
 --

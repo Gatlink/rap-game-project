2de01ce4cd82ba27a155bd8d3c50cb7ce9e504e7
tyganit@gmail.com <tyganit@gmail.com@c389ed42-0118-07df-e2f5-b6c3ece5d433>
2013-10-26 12:27:08 +0000

    - Background integration - Version 0.5: sound, background assets, round, scoring

--- trunk/trunk/Settings.lua
+++ trunk/trunk/Settings.lua
@@ -3,11 +3,17 @@ Settings = {}
 Settings.ScreenWidth  = 1280
 Settings.ScreenHeight =  720
 
+Settings.StreetCredWidth = 430
+Settings.StreetCredHeight = 61
+
 Settings.HitzoneWidthRatio = 0.15
+Settings.HitzoneHeight = 355
+Settings.HitzoneBorderWidth = 45
+Settings.HitzoneYOffset = 116
 
 Settings.NoteSize = 32
 Settings.NoteStartLeftX = 0
 Settings.NoteStartRightX = Settings.ScreenWidth
-Settings.NoteMinOffsetY = 300
-Settings.NoteMaxOffsetY = 500
+Settings.NoteMinOffsetY = 120
+Settings.NoteMaxOffsetY = 470
 Settings.NoteMarginY = 35
